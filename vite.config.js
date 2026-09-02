4ad049692530aeadb0d6bfb9cccbbfb1e1c3abb6
Daniel Tsai <32005886+RSheepDV@users.noreply.github.com>
2026-09-01 19:39:46 -0700

    pages

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+// Base path for deploying to GitHub Pages at
+// https://RSheepDV.github.io/AGPWeeklyTaskSubmitter/
+export default defineConfig({
+  base: '/AGPWeeklyTaskSubmitter/'
+})
